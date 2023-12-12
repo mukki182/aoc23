a2df599c0efe52689ec72b55ee486d87aaa30d6a
Mike <git@mike-giese.de>
2023-12-12 17:37:31 +0100

    day 9

---
+++ 09/run2.php
@@ -0,0 +1,42 @@
+<?php
+
+$handle = fopen('input.txt', 'rb');
+$histories = [];
+
+while ($line = fgets($handle)) {
+    $line = trim($line);
+    $histories [] = explode(' ', $line);
+}
+
+$total = 0;
+
+foreach ($histories as $history) {
+    $rounds = [$history];
+    while (true) {
+        $newHistory = [];
+        $notNull = false;
+        for ($i = 0, $count = count($history) - 1; $i < $count; $i++) {
+            $nextValue = $history[$i + 1] - $history[$i];
+            $newHistory[] = $nextValue;
+            if ($nextValue !== 0) {
+                $notNull = true;
+            }
+        }
+        if ($notNull) {
+            $history = $newHistory;
+            $rounds[] = $newHistory;
+        } else {
+            break;
+        }
+    }
+
+    $interpol = 0;
+
+    for ($i = count($rounds) - 1; $i >= 0; $i--) {
+        $interpol = $rounds[$i][0] - $interpol;
+    }
+
+    $total += $interpol;
+}
+
+echo $total;
